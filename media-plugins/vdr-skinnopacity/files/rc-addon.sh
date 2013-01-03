8706e79012d83178ba78ed3b26e7ad96d65688be
Lucian Muresan <lucianm@users.sourceforge.net>
2013-01-03 04:21:28 +0100

    corrected rc-addon for skinnopacity

--- media-plugins/vdr-skinnopacity/files/rc-addon.sh
+++ media-plugins/vdr-skinnopacity/files/rc-addon.sh
@@ -3,12 +3,8 @@
 # rc-addon-script for plugin skinnopacity
 #
 
-SKINNOPACITY_ICONDIR="/usr/share/vdr/plugins/skinnopacity/icons/"
-SKINNOPACITY_LOGODIR="/usr/share/channel-logos/dvbviewer/"
-SKINNOPACITY_EPGIMGDIR="/video/epg_image_cache/"
-
 plugin_pre_vdr_start() {
-  add_plugin_param "--iconpath=${SKINNOPACITY_ICONDIR}"
-  add_plugin_param "--logopath=${SKINNOPACITY_LOGODIR}"
-  add_plugin_param "--epgimages=${SKINNOPACITY_EPGIMGDIR}"
+  add_plugin_param "--iconpath=${SKINNOPACITY_ICONDIR:=/usr/share/vdr/plugins/skinnopacity/icons/}"
+  add_plugin_param "--logopath=${SKINNOPACITY_LOGODIR:=/usr/share/channel-logos/dvbviewer/}"
+  add_plugin_param "--epgimages=${SKINNOPACITY_EPGIMGDIR:=/video/epg_image_cache/}"
 }
