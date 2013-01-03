661be45a23cf16a3443045af0c8d642f4bddc12a
Lucian Muresan <lucianm@users.sourceforge.net>
2013-01-03 04:25:21 +0100

    corrected rc-addon for vdr-tvguide

--- media-plugins/vdr-tvguide/files/rc-addon.sh
+++ media-plugins/vdr-tvguide/files/rc-addon.sh
@@ -3,10 +3,7 @@
 # rc-addon-script for plugin tvguide
 #
 
-TVGUIDE_LOGODIR="/usr/share/channel-logos/dvbviewer/"
-TVGUIDE_EPGIMGDIR="/video/epg_image_cache/"
-
 plugin_pre_vdr_start() {
-  add_plugin_param "--logodir=${TVGUIDE_LOGODIR}"
-  add_plugin_param "--epgimages=${TVGUIDE_EPGIMGDIR}"
+  add_plugin_param "--logodir=${TVGUIDE_LOGODIR:=/usr/share/channel-logos/dvbviewer/}"
+  add_plugin_param "--epgimages=${TVGUIDE_EPGIMGDIR:=/video/epg_image_cache/}"
 }
