da3cf7b91227b42f5d9676b5f90816420ecec761
Lucian Muresan <lucianm@users.sourceforge.net>
2016-02-29 23:23:23 +0100

    updated media-tv/oscam service script, config, unit with Helau's version

--- media-tv/oscam/files/oscam_watchdog.sh
+++ media-tv/oscam/files/oscam_watchdog.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 #
-# Watchdog for OSCAM by 3PO
+# Watchdog for OSCAM
 
 #set -x
 
 # read variables
 source /etc/conf.d/oscam
-
-
+# check if already running
+if [ "$(pidof -x "$0" -o $$ -o $PPID -o %PPID)" != "" ] ; then
+   logger -s "ERROR: $0 laeuft bereits"
+   exit
+fi
 LOGDIR="/var/log/oscam"
 LOGFILE="oscam_watchdog.log"
 EXITLOG="$LOGDIR/exitlog_$(date +%F_%R).log"
 OSCAMLOG="$LOGDIR/oscam.log"
 
-
-[ ! -d "$LOGDIR" ] && mkdir $LOGDIR
+[ ! -d "$LOGDIR" ] && mkdir -p $LOGDIR
 LOG="${LOGDIR}/${LOGFILE}"
 
-sleep 10
-
-while sleep $CHECKTIME
-do
-	PID=`pidof $DAEMON`
-	if [ -z "$PID" ] ; then
-		echo "$(date) Error, ${DESCRIPTION} is not running" >> $LOG
-		echo "$(date) Restarting ${DESCRIPTION} ..." >> $LOG
-		tail -n50 $OSCAMLOG > $EXITLOG
-		/etc/init.d/$DAEMON restart
-		PID=`pidof $DAEMON`
-		if [ -z "$PID" ] ; then
-			echo "$(date) Error, while restarting ${DESCRIPTION}" >> $LOG
-		else
-			echo "$(date) ${DESCRIPTION} restarted" >> $LOG
-		fi
-	fi
+while [ 1 ] ; do
+   ${OSCAM_DAEMON} ${OSCAM_OPTS}
+   echo "$(date) Error, ${DESCRIPTION} stopped" >> $LOG
+   echo "$(date) Restarting ${DESCRIPTION} ..." >> $LOG
+   tail -n50 $OSCAMLOG > $EXITLOG
+   sleep 3
 done
 
 exit 0
