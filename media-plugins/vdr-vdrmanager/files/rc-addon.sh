6af92f6549f6a5ee065f710e72aec4d180b7aef2
Lucian Muresan <lucianm@users.sourceforge.net>
2025-11-11 00:32:08 +0100

    bumped media-video/vdr-vdrmanager-0.15

--- media-plugins/vdr-vdrmanager/files/rc-addon.sh
+++ media-plugins/vdr-vdrmanager/files/rc-addon.sh
@@ -1,9 +1,11 @@
-# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-vdrmanager/files/rc-addon.sh,v 1.5 2012/12/02 08:39:56 hd_brummy Exp $
 #
 # rc-addon plugin-startup-skript for vdr-vdrmanager
 #
 
 : ${VDRMANAGER_PORT:=6420}
+
+# default path from ebuild merge; no option in config file to overwrite
+: ${VDRMANAGER_CERTFILE:=/etc/vdr/plugins/vdrmanager/vdrmanager.pem}
 
 if [[ -z ${VDRMANAGER_PASS} ]]; then
 	eerror "Empty password in /etc/conf.d/vdr.vdrmanager"
@@ -14,10 +16,13 @@ plugin_pre_vdr_start() {
 
 	add_plugin_param "-p${VDRMANAGER_PORT}"
 	add_plugin_param "-P${VDRMANAGER_PASS}"
-	add_plugin_param "-P${VDRMANAGER_COMPRESSION}"
 
 	if yesno ${SVDRPHOSTS_CHECK:-no}; then
 		add_plugin_param "-s"
 	fi
-}
 
+	add_plugin_param "-k ${VDRMANAGER_CERTFILE}"
+
+	# vdrmanager_compression
+	add_plugin_param "-c ${VDRMANAGER_COMPRESSION}"
+}
